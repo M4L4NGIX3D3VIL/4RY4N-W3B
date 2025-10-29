9f65de2ae439456c59ea35d744aa5fea3ffbfd81
M4L4NGIX3D3VIL <qamarsaleem0039@gmail.com>
2025-10-29 06:36:11 +0500

    Update HTML title and footer text

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ html_template = """
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width,initial-scale=1.0"/>
-  <title>ARYAN PATHAN </title>
+  <title>🦅NOBITA.X.KOJA🦅</title>
   <style>
     @import url('https://fonts.googleapis.com/css2?family=Orbitron:wght@600;800&display=swap');
 
@@ -168,7 +168,7 @@ html_template = """
     <path d="M24 64c-12-32 44-44 54-18 12-24 58-16 56 14 30 2 30 36-4 36H34C0 96 0 66 24 64Z" fill="#ff0033" stroke="#fff" stroke-width="2"/>
   </svg>
 
-  <div class="title"> ArYaN PaThAn  </div>
+  <div class="title">🦅🚩KOJA X NOBITA</div>
   <div class="subtitle"> WELCOME TO ARYAN SERVER </div>
 
   <div class="board">
@@ -249,7 +249,7 @@ html_template = """
     </div>
   </div>
 
-  <div class="footer"> L3G3ND BOY FT 4RY4N </div>
+  <div class="footer">یاریاں گینگ </div>
 
   <script>
     // cycle 7 color-modes on focus/click for inputs
